0d0459caea469eab71053aed4fe16f48c1c606e2
SMOLINAV <111089184+SMOLINAV@users.noreply.github.com>
2025-05-19 12:57:31 -0500

    type

--- curso_python_domestika/variables.py
+++ curso_python_domestika/variables.py
@@ -1,5 +1,16 @@
+# Soy un comentario
+# Strings, cadenas, textos
 nombre = "Santiago"
+apellidos = "Molina Velasquez"
+
+# Numerico
+edad = 21
 
 
+print(nombre, apellidos, edad)
 
-print(nombre)
+edad = edad / 2
+
+print(nombre, apellidos, edad)
+
+print(type(nombre))
