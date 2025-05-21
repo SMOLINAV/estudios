b72b7827fdb54a10a58811492efe5a869d434687
SMOLINAV <111089184+SMOLINAV@users.noreply.github.com>
2025-05-20 22:04:58 -0500

    condicionales 1

---
+++ curso_python_domestika/condicionales.py
@@ -0,0 +1,14 @@
+# logico boolean booleano
+# Verdadero, falso: True, False
+# <> !=
+# == igual
+# > mayor
+# < menor
+
+nombre = input("por favor ingresa tu nombre: ")
+if nombre == "santiago":
+    print("que hermoso nombre")
+else:
+    print("que raro nombre")
+
+print("fin del programa")
